d35edb3e65c232dbaf2143f73fb73ac5b52af405
Oro-Kin <137208412+Oro-Kin@users.noreply.github.com>
2023-07-10 14:21:17 -0400

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,44 @@
+let chapterOneLink = document.querySelector(".chapter1");
+let chapterTwoLink = document.querySelector(".chapter2");
+let chapterThreeLink = document.querySelector(".chapter3");
+let chapterFourLink = document.querySelector(".chapter4");
+
+
+
+function chapterOne() {
+    console.log("test");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+
+}
+
+function chapterTwo() {
+    console.log("test");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+
+}
+
+function chapterThree() {
+    console.log("test");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+
+}
+
+function chapterFour() {
+    console.log("test");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+
+}
+function chapterOne1() {
+    console.log("test-1");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+}
+function chapterTwo1() {
+    console.log("test-1");
+    let pageAnimation = document.querySelector(".pageturnani")
+    pageAnimation.style.display= "inline-flex";
+}
